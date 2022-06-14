9133c204ca9e6c35250d1d5110bbe1c2dc89f461
sungwookoo <sungwookoo.dev@gmail.com>
2022-06-15 08:06:14 +0900

    Added isort

--- djangoPoetry/settings.py
+++ djangoPoetry/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from typing import List
 
